bbbfe5e7c3cfae436f804c3974fa91f4d7c4b511
Stanislav Gumeniuk <vigo@vigo.su>
2012-02-20 09:34:25 +0400

    second reg work

--- ereg.php
+++ ereg.php
@@ -1,8 +1,14 @@
 <?
 $url = "http://www.mysite.com/section/index/php";
 
-$regexp = "[A-a]";
+$regexp = '/[^.]+\.[^.]+$/';
 
 preg_match($regexp, $url, $matches);
 
 var_dump( $matches);
+
+$regexp2 = '@^(?:http://)?([^/]+)@i';
+
+preg_match($regexp2,$url, $matches2);
+
+var_dump($matches2);
