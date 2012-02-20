5a763be78f2c9cd897ff054ae740d969a5aae6cf
Stanislav Gumeniuk <vigo@vigo.su>
2012-02-20 10:10:36 +0400

    t

--- cook.php
+++ cook.php
@@ -1,3 +1,12 @@
 <?
-
+$_COOKIE["qqq"]="fff";
 setcookie("name", "love");
+
+echo $_COOKIE["name"];
+
+echo "<html><head> 
+	<meta http-equiv=\"Set-Cookie\" content=\"name1=qwe;\">
+	</head>
+</html>	
+";
+
