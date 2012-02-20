61ee21651ad740b15a7811a6a940f499cc3dd5fb
Stanislav Gumeniuk <vigo@vigo.su>
2012-02-20 13:55:19 +0400

    done

--- ereg.php
+++ ereg.php
@@ -1,13 +1,15 @@
 <?
 $url = "http://www.mysite.com/section/index/php";
-
+$url="http://dfhfdfg/";
 $regexp = '/[^.]+\.[^.]+$/';
 
 preg_match($regexp, $url, $matches);
 
-var_dump( $matches);
+#var_dump( $matches);
 
-$regexp2 = '@^(?:http://)?([^/]+)@i';
+#$regexp2 = '@^(?:http://)?([^/]+)@i';
+#$regexp2 = '@^(?:http://)([a-z.]*)([^/]+)@';
+$regexp2  = '/^(?:http:\/\/)+([^\/]+)/';
 
 preg_match($regexp2,$url, $matches2);
 
